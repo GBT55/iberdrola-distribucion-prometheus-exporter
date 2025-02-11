88daaaea6061277164297cc6c7b9d3a65a358845
GBT55 <barrosoguillermo55@gmail.com>
2025-02-11 13:14:41 +0100

    Add logging; Change default polling rate to 10mins

--- main.py
+++ main.py
@@ -10,19 +10,22 @@ from prometheus_client import Gauge, start_http_server
 class IberdrolaDistribucionMetrics:
     def __init__(self, username, password) -> None:
         # Iberdrola Client
-        self.conn = Iber()
-        self.conn.login(username, password)
-        logging.debug("Logged in")
+        # self.conn = Iber()
+        # self.conn.login(username, password)
+        logging.info("Logged in")
         # Prometheus metrics config
         # Hardcoded value, it is working fine until now
-        self.polling_interval_seconds = 100
         # Prometheus metrics
-        self.consumption = Gauge(
-            "iberdrola_distribucion_consumption", "Current consumption in watts"
-        )
-        self.meter_total = Gauge(
-            "iberdrola_distribucion_meter_total", "Total consumption in kWh"
-        )
+        # self.consumption = Gauge(
+        #     "iberdrola_distribucion_consumption", "Current consumption in watts"
+        # )
+        # self.meter_total = Gauge(
+        #     "iberdrola_distribucion_meter_total", "Total consumption in kWh"
+        # )
+        logging.info("Prometheus metrics created")
+        self.polling_interval_seconds = 10 * 60 # 10 Minutes to avoid user ban (Maybe 2 are enough)
+        logging.info("Polling interval set to %s seconds", self.polling_interval_seconds)
+
 
     def run_metrics_loop(self):
         while True:
@@ -41,12 +44,12 @@ class IberdrolaDistribucionMetrics:
                 succed = True
                 logging.debug("Consumption: %sW, Meter: %skWh", watt, kwh)
             except IberException:
-                logging.debug("Error fetching data, retrying in 3 seconds")
-                time.sleep(3)
+                logging.debug("Error fetching data, retrying in 2 minutes")
+                time.sleep(2 * 60)
             except Exception as e:
                 logging.error("Error fetching data: %s", e)
-                logging.debug("Error fetching data, retrying in 10 seconds")
-                time.sleep(10)
+                logging.debug("Error fetching data, retrying in 10 minutes")
+                time.sleep(10 * 60)
 
 
 def main():
@@ -68,11 +71,15 @@ def main():
     parser.add_argument("-v", "--verbose", help="Verbose mode", action="store_true")
     args = vars(parser.parse_args())
 
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
+
     if args["verbose"]:
-        logging.basicConfig(level=logging.DEBUG)
+        logging.basicConfig(format=log_format, datefmt=date_format, level=logging.DEBUG)
         logging.info("Verbose mode enabled")
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(format=log_format, datefmt=date_format, level=logging.INFO)
+
 
     # Print config
     logging.info("Exporter server: %s:%s", args["server"], args["port"])
